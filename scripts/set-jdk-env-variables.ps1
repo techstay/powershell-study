bff693b0ab7b94f7679a654423a6ab79d1c11127
yitian <lovery521@gmail.com>
2017-11-08 01:19:46 +0800

    去除了在java9中不存在的类路径

--- scripts/set-jdk-env-variables.ps1
+++ scripts/set-jdk-env-variables.ps1
@@ -20,7 +20,7 @@ Write-Host "发现安装位置: ${install_location}"
 
 # 配置环境变量
 $java_home = $install_location
-$classpath = "%JAVA_HOME%\lib;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar"
+$classpath = "%JAVA_HOME%\lib"
 $path = "%JAVA_HOME%\bin;" + $env:Path
 
 [Environment]::SetEnvironmentVariable("JAVA_HOME", $java_home, "Machine")
