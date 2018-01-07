4e80fb8409a8bda19cd1e1afad66a7a70b2f3a97
yitian <lovery521@gmail.com>
2018-01-07 17:49:12 +0800

    添加了随机数信息

--- scripts/fgo-draw.ps1
+++ scripts/fgo-draw.ps1
@@ -1,4 +1,7 @@
 while ($true) {
-    adb shell input tap 700 700
-    Start-Sleep -Milliseconds 300
+    $dx = Get-Random -Maximum 20 -Minimum 0
+    $dy = Get-Random -Maximum 20 -Minimum 0
+    $dt = Get-Random -Minimum 100 -Maximum 400
+    adb shell input tap $(700 + $dx) $(700 + $dy)
+    Start-Sleep -Milliseconds $(300 + $dt)
 }
